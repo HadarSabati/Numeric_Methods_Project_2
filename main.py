75771c873329e4fa5e37d8645394f7c533f7a224
HadarSabati <75303359+HadarSabati@users.noreply.github.com>
2021-05-14 14:25:08 +0300

    part 1 A

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+
+from numpy import asarray
+from numpy.random import rand
+from numpy.random import randn
 from mpl_toolkits.mplot3d import axes3d
 # matplotlib inline
 
@@ -18,6 +22,7 @@ r = 0.3
 WsArr = [1, r, 0.95 * r, 0.90 * r, 0.85 * r, 0.8 * r]
 
 gaussianFunctions = []
+objectiveFunctions = []
 x = np.arange(-10, 10, 0.025)
 y = np.arange(-10, 10, 0.025)
 X, Y = np.meshgrid(x, y)
@@ -32,6 +37,9 @@ for i in range(6):
     Z = WsArr[i]*((np.exp(-(X - xExp) ** 2 / (2 * xVar)))*(np.exp(-(Y - yExp) ** 2 / (2 * yVar))))
     gaussianFunctions.append(Z)
 
+    objective = WsArr[i]*((e ** (-(X - xExp) ** 2 / (2 * xVar)))  *  (e ** (-(Y - yExp) ** 2 / (2 * yVar))))
+    objectiveFunctions.append(objective)
+
 final = np.maximum(gaussianFunctions[0], gaussianFunctions[1])
 final = np.maximum(final, gaussianFunctions[2])
 final = np.maximum(final, gaussianFunctions[3])
@@ -41,28 +49,95 @@ final = np.maximum(final, gaussianFunctions[5])
 mycmap = plt.get_cmap('turbo')
 fig = plt.figure(figsize=(15, 15))
 axes = fig.add_subplot(111, projection='3d')
+
+axes.scatter(1,1,1,color='g')
+axes.scatter(4,4,1,color='g')
+axes.scatter(2,1,0.5,color='g')
+axes.scatter(6,2,0.1,color='g')
+
 surf1 = axes.plot_surface(X, Y, final, cmap=mycmap)
 axes.set_title('Gaussian Graph')
 fig.colorbar(surf1, ax=axes) # , shrink=0.5, aspect=10)
 
 
-print(final)
+def objective(v):
+
+
+
+# check if a point is within the bounds of the search
+def in_bounds(point, bounds):
+    # enumerate all dimensions of the point
+    for d in range(len(bounds)):
+        # check if out of bounds for this dimension
+        if point[d] < bounds[d, 0] or point[d] > bounds[d, 1]:
+            return False
+    return True
+
+
+# hill climbing local search algorithm
+def hillClimbing(objective, bounds, n_iterations, step_size, start_pt):
+    # store the initial point
+    solution = start_pt
+    # evaluate the initial point
+    solution_eval = objective(solution)
+    # run the hill climb
+    for i in range(n_iterations):
+        # take a step
+        candidate = None
+        while candidate is None or not in_bounds(candidate, bounds):
+            candidate = solution + randn(len(bounds)) * step_size
+        # evaluate candidate point
+        candidte_eval = objective(candidate)
+        # check if we should keep the new point
+        if candidte_eval >= solution_eval:
+            # store the new point
+            solution, solution_eval = candidate, candidte_eval
+            print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
+    return [solution, solution_eval]
+
+
+
+# # hill climbing local search algorithm
+# def hillClimbing(objective, bounds, n_iterations, step_size):
+#     # generate an initial point
+#     solution = None
+#     while solution is None or not in_bounds(solution, bounds):
+#         solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+#     # evaluate the initial point
+#     solution_eval = objective(solution)
+#     # run the hill climb
+#     for i in range(n_iterations):
+#         # take a step
+#         candidate = None
+#         while candidate is None or not in_bounds(candidate, bounds):
+#             candidate = solution + randn(len(bounds)) * step_size
+#         # evaluate candidate point
+#         candidte_eval = objective(candidate)
+#         # check if we should keep the new point
+#         if candidte_eval <= solution_eval:
+#             # store the new point
+#             solution, solution_eval = candidate, candidte_eval
+#             # report progress
+#             print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
+#     return [solution, solution_eval]
+
+
+bounds = asarray([[-5.0, 5.0], [-5.0, 5.0]])
+hillClimbing(objective, bounds, 10, 0.05, (2,5))
+
 
 plt.show()
-
-
-
 
 
 #
 # ## Setting parameters
 # obj_func = F5
 # verbose = False
-# epoch = 500
-# pop_size = 50
-#
-# # A - Different way to provide lower bound and upper bound. Here are some examples:
-#
+# epoch = 50
+# pop_size = 10
+
+# A - Different way to provide lower bound and upper bound. Here are some examples:
+
 # ## 1. When you have different lower bound and upper bound for each parameters
 # lb1 = [-3, -5, 1]
 # ub1 = [5, 10, 100]
@@ -70,7 +145,7 @@ plt.show()
 # md1 = BaseHC(obj_func, lb1, ub1, verbose, epoch, pop_size)
 # best_pos1, best_fit1, list_loss1 = md1.train()
 # print(md1.solution[1])
-#
+
 # ## 2. When you have same lower bound and upper bound for each parameters, then you can use:
 # ##      + int or float: then you need to specify your problem size (number of dimensions)
 # problemSize = 10
@@ -79,7 +154,7 @@ plt.show()
 # md2 = BaseHC(obj_func, lb2, ub2, verbose, epoch, pop_size, problem_size=problemSize)  # Remember the keyword "problem_size"
 # best_pos1, best_fit1, list_loss1 = md2.train()
 # print(md2.solution[1])
-#
+
 # ##      + array: 2 ways
 # lb3 = [-5]
 # ub3 = [10]
@@ -92,9 +167,9 @@ plt.show()
 # md4 = BaseHC(obj_func, lb4, ub4, verbose, epoch, pop_size)  # No need the keyword "problem_size"
 # best_pos1, best_fit1, list_loss1 = md4.train()
 # print(md4.solution[1])
-#
-# # B - Test with algorithm has batch size idea
-#
+
+# B - Test with algorithm has batch size idea
+
 # ## 1. Not using batch size idea
 #
 # md5 = BaseHC(obj_func, lb4, ub4, verbose, epoch, pop_size)
